9831f7ef6d166ba29c4322330dd780c1e805d2c6
zGrizzy <159240805+zGrizzy@users.noreply.github.com>
2024-04-26 10:21:53 +0300

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -10,7 +10,7 @@ def plot_temperature_comparison(data):
     plt.title('Temperature Comparison Among Cities')
     plt.xlabel('Date')
     plt.ylabel('Temperature (°C)')
-    plt.xticks(rotation=25)
+    plt.xticks(rotation=-55)
     plt.legend(title='City')
     plt.show()
 
@@ -18,5 +18,5 @@ def plot_interactive_temperature_comparison(data):
     fig = px.line(data, x='Date', y='Temperature', color='City', title='Temperature Comparison Among Cities',
                   markers=True)
     fig.update_yaxes(title='Temperature (°C)', tickformat=".1f° C")
-    fig.update_xaxes(tickangle=0)
+    fig.update_xaxes(tickangle=45)
     fig.show()
