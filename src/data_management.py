47bac74bed0122155a2189471cbd36c238192f6d
zGrizzy <159240805+zGrizzy@users.noreply.github.com>
2024-05-02 06:28:33 +0300

    Update data_management.py

--- src/data_management.py
+++ src/data_management.py
@@ -2,19 +2,21 @@ import requests
 
 
 def get_weather(city):
-    api_key = ""
+    api_key = "2ff8e9d24a2cc365df35ebf3795b8af3"
     base_url = "http://api.openweathermap.org/data/2.5/weather?"
     full_url = f"{base_url}appid={api_key}&q={city}&units=metric"
     response = requests.get(full_url)
     data = response.json()
-    if data["cod"] == 200:
+
+    if data['cod'] == 200:
         weather_info = {
-            'City': city,
-            'Temperature': data["main"]["temp"],
-            'Pressure': data["main"]["pressure"],
-            'Humidity': data["main"]["humidity"],
-            'Wind': data.get('wind', {}).get('speed', 'N / A'),
-            'Description': data["weather"][0]["description"],
+            'Temperature': data['main']['temp'],
+            'City': data['name'],
+            'Country': data['sys']['country'],
+            'Pressure': data['main']['pressure'],
+            'Humidity': data['main']['humidity'],
+            'Wind': data['wind']['speed'],
+            'Description': data['weather'][0]['description']
         }
         return weather_info
     else:
